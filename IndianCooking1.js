cf1ac954913217f8309f52657ecf0e04ee0f4f62
Shimanti <shimanti@gmail.com>
2021-07-30 04:54:40 -0700

    added node files

---
+++ IndianCooking1.js
@@ -0,0 +1,637 @@
+let IndianRecipes = {
+"Recipe1": {
+"Title":"Easy Indian Fried Rice ",
+"Serves":"(Serves 4-6) ",
+"Ingredients":
+[
+"1 tbsp Vegetable Oil ",
+"Whole Garam Masala: ",
+"  2-3 Bayleaf ",
+"  4-5 Cloves ",
+"  ½ tsp Cardamom Seeds (Elachi) ",
+"  4 Cinnamon Sticks ",
+"  ½ tsp Cumin Seeds ",
+"1 cup Frozen Vegetables ",
+"2 tsp Ghee/Butter ",
+"2 cups Rice ",
+"1 tsp Salt ",
+"2 tsp Sugar ",
+],
+"Directions":
+[
+"Wash rice. Drain all water. Set aside. ",
+"Heat oil in pan. ",
+"Fry whole garam masala in hot oil for a few seconds. ",
+"Add frozen vegetables and fry over medium heat until water has evaporated. ",
+"Add ghee, rice, salt and sugar. ",
+"Fry for 5 minutes. ",
+"Add water and transfer to rice cooker. ",
+"Add nutmeg and mace. ",
+"Stir and set cooker on. "
+]},
+"Recipe2":{
+"Title":"Masoori Dahl ",
+"Serves":"(Serves 6-8) ",
+"Ingredients":
+[
+"1 cup Masoori Dahl ",
+"4 cups Water ",
+"4-6 Garlic Cloves ",
+"4 whole Green Chilies ",
+"1 Tomato (diced) or ",
+" ½ tsp Turmeric ",
+"2 cups Water ",
+"1 tsp Salt ",
+"2 tbsp Lemon Juice ",
+"¼ cup Cilantro (chopped) ",
+"1 tbsp Vegetable Oil or Butter ",
+"1 Onion (finely chopped) ",
+"½ tsp Kalogiray"
+],
+"Directions":
+[
+"Wash dahl. ",
+"Pressure cook 4 cups water, dahl, garlic, chilies, and tomato (or turmeric). ",
+"Boil for 2 minutes after whistling. ",
+"Release pressure immediately. ",
+"Add 2 cups water and stir. ",
+"Add salt, lemon juice and cilantro. ",
+"Boil for 1 minute over low heat. ",
+"In a small frying pan, heat oil or butter. ",
+"Fry onions until brown. Add kalogiray and mix. ",
+"Pour spice and onions into dahl. Stir and serve."
+]},
+"Recipe3": {
+"Title":"Chick Pea Salad ",
+"Serves":"(Serves 4-6) ",
+"Ingredients":
+[
+"1 16oz tin Garbanzos/ Chick Peas ",
+"1 Red Onion (cut into small wedges) ",
+"2 Potatoes (baked in microwave, peeled ",
+"and cut small) ",
+"1 Tomato (diced) ",
+"½ cup Cilantro (chopped) ",
+"2 tsp Chat Masala ",
+"1 Lemon (juice only) "
+],
+"Directions":
+[
+"Throw water of chick peas away. ",
+"Put chick peas in large bowl. ",
+"Add all ingredients. ",
+"Mix and serve. ",
+]},
+"Recipe4": {
+"Title":"Cauliflower and Tomatoes ",
+"Serves":"(Serves 4) ",
+"Ingredients":
+[
+"1 tbsp Vegetable Oil ",
+"1 large Onion (chopped) ",
+"1 Cauliflower (cut small) ",
+"2 Potatoes (cut small) ",
+"1-2 Chilies ",
+"½ tsp Salt ",
+"1 cup Frozen Peas ",
+"1 large Tomato (diced) ",
+"1 tsp Sugar ",
+"½ tsp Kalogiray ",
+"½ cup Cilantro ",
+],
+"Directions":
+[
+"Heat oil in large skillet. ",
+"Add onion. Fry till brown. ",
+"Add cauliflower, potatoes, chilies and salt. Fry for 5 minutes. ",
+"Add peas and tomato. Cover, put on low heat and simmer for minutes. ",
+"Check regularly and stir to prevent burning. ",
+"Add sugar, kalogiray and cilantro before bringing off heat. ",
+]},
+"Recipe5": {
+"Title":"Cauliflower Curry ",
+"Serves":"(Serves 4-6) ",
+"Ingredients":
+[
+"2 tbsp Vegetable Oil ",
+"Whole Garam Masala: ",
+"  1 Bayleaf ",
+"  4 Cloves ",
+"  3-4 Cardamom Pods ",
+"  ½ tsp Cardamom Seeds (Elachi) ",
+"  4 Cinnamon Sticks ",
+"1 Cauliflower (cut small) ",
+"1-2 Potatoes (cut small) ",
+"1 tsp Turmeric ",
+"1 tsp Cumin (ground) ",
+"1 tsp Chili Powder ",
+"1 tsp Coriander Powder ",
+"½ cup Frozen Peas ",
+"1 large Tomato (diced) ",
+"1 tsp Salt ",
+"2 tsp Sugar ",
+"1 cup Water ",
+"1 tsp Garam Masala (ground) ",
+],
+"Directions":
+[
+"Heat oil in large skillet. ",
+"Fry whole garam masala for a few seconds. ",
+"Add cauliflower and potatoes. Fry for 5-minutes. ",
+"Add turmeric, cumin, chili powder, and coriander powder. Fry spices. ",
+"Add peas and tomato. Mix and fry. ",
+"Add salt and sugar. Mix and fry for a few seconds. ",
+"Add water. Mix. ",
+"Cover, put on low heat and simmer for minutes. ",
+"Check regularly and stir to prevent burning. ",
+"Add garam masala before bringing off heat. ",
+]},
+"Recipe6": {
+"Title":"Cabbage Curry ",
+"Serves":"(Serves 4-6) ",
+"Ingredients":
+[
+"1 tbsp Vegetable Oil ",
+"Whole Garam Masala: ",
+"  1 Bayleaf ",
+"  4 Cloves ",
+"  3-5 Cardamom Pods ",
+"  4 Cinnamon Sticks ",
+"  ½ tsp Cardamom Seeds (Elachi) ",
+"1-2 Potatoes (cut small) ",
+"1 Tomato (diced) ",
+"1 tsp Turmeric ",
+"1 tsp Cumin (ground) ",
+"1 tsp Chili Powder ",
+"1 tsp Coriander Powder ",
+"1 Cabbage (diced) ",
+"½ cup Frozen Peas ",
+"½ tsp Salt ",
+"1 tsp Sugar ",
+"1 tsp Garam Masala (ground) ",
+],
+"Directions":
+[
+"Heat oil in large skillet. ",
+"Fry whole garam masala for a few seconds. ",
+"Add potatoes, tomatoes, turmeric, cumin, coriander, and chili powder. Fry. ",
+"Add cabbage, peas, salt and sugar. Mix and fry for a couple of seconds. ",
+"Cover, put on low heat and simmer for minutes. ",
+"Dry if needed. ",
+"Add garam masala before bringing off heat. ",
+"Taste before serving.",
+]},
+"Recipe7": {
+"Title":"Lemony Chicken ",
+"Serves":"(Serves 4) ",
+"Ingredients":
+[
+"1 tbsp Vegetable Oil ",
+"1in Ginger (grated) ",
+"8 Garlic Cloves (crushed) ",
+"8 Chicken Pieces ",
+"1 tsp Salt ",
+"½ cup Water ",
+"2 cups Cilantro (chopped) ",
+"1 whole Lemon ",
+],
+"Directions":
+[
+"Heat oil in pressure cooker. ",
+"Add ginger and garlic. Fry for a few seconds. ",
+"Add chicken. Fry until brown. ",
+"Add cilantro, salt, and water. ",
+"Pressure cook for 5 minutes. ",
+"Open and add lemon juice. ",
+"Add more cilantro, if needed. "
+]},
+"Recipe8": {
+"Title":"Red Curry Chicken Breast ",
+"Serves":"(Serves 2) ",
+"Ingredients":
+[
+"1 tsp Vegetable Oil ",
+"Whole Garam Masala: ",
+"  1 Bayleaf ",
+"  2 Cloves ",
+"  2 Cardamom Pods ",
+"  2 Cinnamon Sticks ",
+"1 Potato (cut into 4 pieces) ",
+"1 Handful Peas ",
+"1 Tomato (diced) ",
+"1 Chicken Breast (cut into pieces) ",
+"1 tsp Red Curry Paste ",
+"1-cup water ",
+],
+"Directions":
+[
+"Heat oil in pressure cooker. ",
+"Add whole garam masala. Fry till smell arises. ",
+"Add potatoes, peas, tomato, and chicken. Fry for 2 mins. ",
+"Add red curry paste and water. ",
+"Pressure cook for 5 mins. ",
+]},
+"Recipe9": {
+"Title":"Tomato Chicken (Normal) ",
+"Serves":"(Serves 4) ",
+"Ingredients":
+[
+"1 tbsp Vegetable Oil ",
+"½ Onion (diced) ",
+"1 in Ginger (grated) ",
+"8 Garlic Cloves (crushed) ",
+"1 tsp Paprika ",
+"½ tsp Cumin (ground) ",
+"½ tsp Chili Powder ",
+"8 Chicken Pieces ",
+"1 tsp Salt ",
+"2 Tomatoes (large) ",
+"1 tsp Butter "
+],
+"Directions":
+[
+"Heat oil in pressure cooker. ",
+"Add onion. Fry till brown. ",
+"Add ginger and garlic. Fry a little. ",
+"Add paprika, cumin, and chili powder. Mix. ",
+"Add chicken and salt. Fry for 2 minutes. ",
+"Add tomatoes. Fry on high for 5 minutes. ",
+"Pressure cook for 2 minutes. ",
+"Release pressure. ",
+"Add a pat of butter. ",
+"Mix and serve. "
+]},
+"Recipe10": {
+"Title":"Tomato Chicken (Easy) ",
+"Serves":"(Serves 4) ",
+"Ingredients":
+[
+"1 tbsp Vegetable Oil ",
+"½ tsp Ginger (grated) ",
+"2 tsp Garlic Cloves (crushed) ",
+"8 Chicken Pieces ",
+"1 tsp Salt ",
+"2 Tomatoes (large) ",
+],
+"Directions":
+[
+"Heat oil in pressure cooker. ",
+"Add ginger and garlic. Fry a little. ",
+"Add chicken and salt. Fry for 2 minutes. ",
+"Add tomatoes. Fry on high for 5 minutes. ",
+"Pressure cook for 2 minutes. ",
+"Release pressure and serve. ",
+]},
+"Recipe11": {
+"Title":"Tomato Chicken Breast Curry (Easy) ",
+"Serves":"(Serves 2) ",
+"Ingredients":
+[
+"1 tsp Vegetable Oil ",
+"¼ tsp Ginger (grated) ",
+"1 tsp Garlic Cloves (crushed) ",
+"1 Chicken Breast ",
+"1 Tomato ",
+"1 Potato (cut in 4 pieces) ",
+"1 cup Water ",
+"½ tsp Salt "
+],
+"Directions":
+[
+"Heat oil in pressure cooker. ",
+"Add ginger and garlic. Fry a little. ",
+"Add chicken. Fry for 1 minute. ",
+"Add potato and tomato. Fry on high for 2 minutes. ",
+"Add water and salt. ",
+"Pressure cook for 5 minutes. ",
+"Release pressure and serve. "
+]},
+"Recipe12": {
+"Title":"Chicken Curry (Normal) ",
+"Serves":"(Serves 4-6) ",
+"Ingredients":
+[
+"½ Onion (diced) ",
+"1 tsp Ginger (crushed) ",
+"1 tsp Garlic Cloves (crushed) ",
+"1 tbsp Vegetable Oil ",
+"Whole Garam Masala: ",
+"  1 Bayleaf ",
+"  4 Cloves ",
+"  3-6 Cardamom Pods ",
+"  4 Cinnamon Sticks ",
+"½ Onion (diced) ",
+"1 tsp Cumin (ground) ",
+"1 tsp Chili Powder or 1 Chili (sliced) ",
+"2 Potatoes (cut into eighths) ",
+"½-1 Tomato (diced) ",
+"1 Chicken (cut into pieces) ",
+"2-3 Carrots (cut into 1 in pieces) ",
+"½ tsp Salt ",
+"1 cup Water ",
+"1 tsp Lemon Juice ",
+"¼ cup Cilantro (chopped)"
+],
+"Directions":
+[
+"Blend ½ onion, ginger and garlic. Set aside. ",
+"Heat oil in pressure cooker. ",
+"Add whole garam masala. ",
+"Add ½ onion. Fry till brown. ",
+"Add blended mixture. Fry. ",
+"Add cumin and chili. Fry. ",
+"Add potatoes. Fry till brown. ",
+"Add tomato. Fry till color changes. ",
+"Add chicken, and carrots. Fry. ",
+"Add salt. Fry. ",
+"When dry, add water. ",
+"Pressure cook for 5 minutes. ",
+"Release pressure and open. ",
+"Add lemon juice and cilantro. ",
+"Add salt, if needed."
+]},
+"Recipe13": {
+"Title":"Chicken Curry (Easy) ",
+"Serves":"(Serves 4-6) ",
+"Ingredients":
+[
+"1 tbsp Vegetable Oil ",
+"Whole Garam Masala (optional) ",
+"  1 Bayleaf ",
+"  4 Cloves ",
+"  3-7 Cardamom Pods ",
+"  4 Cinnamon Sticks ",
+"1 Onion (diced) ",
+"1 tsp Ginger (crushed) ",
+"1 tsp Garlic Cloves (crushed) ",
+"1 tsp Cumin (ground) ",
+"1 tsp Chili Powder ",
+"1 Tomato (diced) ",
+"1 Chicken (cut into pieces) ",
+"2-3 Carrots (cut into 1 in pieces) (optional) ",
+"½ tsp Salt ",
+"1-cup water ",
+"1 tsp Lemon Juice (optional) ",
+"¼ cup Cilantro (chopped) (optional)"
+],
+"Directions":
+[
+"Heat oil in pressure cooker. ",
+"Add whole garam masala. Fry till smell arises. ",
+"Add onion. Fry till brown. ",
+"Add ginger and garlic. Fry slightly. ",
+"Add cumin and chili. Fry slightly. ",
+"Add tomato. Fry till color changes. ",
+"Add chicken. Fry. ",
+"Add salt, carrots and water. ",
+"Boil and then simmer for minutes. ",
+"Add lemon juice and cilantro. ",
+"Add salt, if needed."
+]},
+"Recipe14": {
+"Title":"Chicken Breast Curry (Easy) ",
+"Serves":"(Serves 2) ",
+"Ingredients":
+[
+"1 tsp Vegetable Oil ",
+"Whole Garam Masala: ",
+"  1 Bayleaf ",
+"  2 Cloves ",
+"  2 Cardamom Pods ",
+"  2 Cinnamon Sticks ",
+"1 Potato (cut into 4 pieces) ",
+"1 Handful Peas ",
+"1 Tomato (diced) ",
+"1 Chicken Breast (cut into pieces) ",
+"1 Carrot (cut into 1 in pieces) (optional) ",
+"½ tsp Salt ",
+"1-cup water "
+],
+"Directions":
+[
+"Heat oil in pressure cooker. ",
+"Add whole garam masala. Fry till smell arises. ",
+"Add potatoes, peas, tomato, and chicken. Fry for 2 mins. ",
+"Add carrot, water and salt. ",
+"Pressure cook for 5 mins. "
+]},
+"Recipe15": {
+"Title":"Yogurt Chicken Curry ",
+"Serves":"(Serves 4-6) ",
+"Ingredients":
+[
+"½ Onion (diced) ",
+"1 in Ginger (grated) ",
+"1 tsp Garlic Cloves (crushed) ",
+"1 ½ tbsp Yogurt ",
+"½ tsp Chili Powder ",
+"1 tsp Salt ",
+"1 tsp Garam Masala (ground) ",
+"1 Chicken (cut into 8 pieces) ",
+"1 tbsp Vegetable Oil ",
+"½ Onion (diced) ",
+"Whole Garam Masala: ",
+"1 Bayleaf ",
+"4 Cloves ",
+"3-8 Cardamom Pods ",
+"4 Cinnamon Sticks ",
+"½ tsp Cumin Seeds ",
+"1 tsp Lemon Juice ",
+"Cilantro leaves"
+],
+"Directions":
+[
+"Blend all ingredients up to ground garam masala. ",
+"Marinate chicken in mixture. ",
+"Heat oil in pressure cooker. ",
+"Add onion. Fry till brown. ",
+"Add whole garam masala. ",
+"Add chicken mixture. ",
+"Pressure cook for 5 minutes. ",
+"Open and add lemon juice. ",
+"Add cilantro, as needed. ",
+]},
+"Recipe16": {
+"Title":"Beef Chops ",
+"Serves":"(Serves 6-8) ",
+"Ingredients":
+[
+"1lb Beef (minced) ",
+"2 tbsp Vegetable Oil ",
+"Whole Garam Masala ",
+"  2-3 Bayleaf ",
+"  4-5 Cloves ",
+"  4-5 Cardamom Pods ",
+"  4 Cinnamon Sticks ",
+"2 Onion (chopped) ",
+"6-7 Garlic Cloves (crushed) ",
+"1in Ginger (grated) ",
+"½ lb Potatoes (cut into eighths) ",
+"½ cup Cilantro (chopped) ",
+"½ cup Water ",
+"1 whole Lemon ",
+"1 tsp Salt ",
+"½ cup Cilantro (chopped) ",
+"½ -1 tsp Chili Powder ",
+"1 Egg (beaten) ",
+"1 cup Breadcrumbs "
+],
+"Directions":
+[
+"Wash beef and put aside. ",
+"Heat oil in pressure cooker. ",
+"Fry whole garam masala in hot oil for a few seconds. ",
+"Add onion. Fry till brown. ",
+"Add garlic, and ginger. Fry for a few seconds or until dry. ",
+"Add minced beef. Fry over medium heat for 5 minutes. ",
+"Add potatoes, water and cilantro. ",
+"Pressure cook for 5 minutes. ",
+"Open and taste. ",
+"Add lemon juice, salt, cilantro and chili powder. Mix. ",
+"Wait to cool. ",
+"Mash mixture together. ",
+"Make oval balls. ",
+"Dip into egg and cover with breadcrumbs. ",
+"Heat oil in a shallow frying pan. ",
+"Fry as many as needed. ",
+"Freeze or refrigerate remaining mixture. "
+]},
+"Recipe17": {
+"Title":"Turkey Keema ",
+"Serves":"(Serves 4) ",
+"Ingredients":
+[
+"1lb Turkey (minced) ",
+"½ Onion (grated) ",
+"1 tsp Garlic Cloves (crushed) ",
+"½ Lime (juice) ",
+"1 tbsp Vegetable Oil ",
+"Whole Garam Masala ",
+"  1 Bayleaf ",
+"  4 Cloves ",
+"  3-4 Cardamom Pods ",
+"  4 Cinnamon Sticks ",
+"½ Onion (chopped) ",
+"2 tsp Garlic Cloves (crushed) ",
+"2 tsp Ginger (grated) ",
+"1 tsp Turmeric ",
+"2 tsp Cumin (ground) ",
+"1 tsp Chili Powder ",
+"½ tsp Salt ",
+"2 tsp Sugar ",
+"1 tsp Coriander Powder ",
+"½ cup Water ",
+"1 tsp Garam Masala (ground) ",
+"4 Carrots (cut into small pieces) ",
+"1 cup Water ",
+"1 tsp Roasted Masala (ground) (optional) "
+],
+"Directions":
+[
+"Marinate first four ingredients together. ",
+"Heat oil in pan. ",
+"Fry whole garam masala in hot oil for a few seconds. ",
+"Add onion. Fry till brown. ",
+"Add garlic, and ginger. Fry for a few seconds or until dry. ",
+"Add turkey mixture and turmeric. ",
+"When water dries and turmeric gets cooked, add cumin, chili powder, salt, sugar and ",
+"coriander powder. Fry until dry. ",
+"Add water, garam masala, peas and carrot. ",
+"Pressure cook for 2 mins. ",
+"Boil down till desired amount of gravy is left. ",
+"Add roasted masala. ",
+"Mix and serve. "
+]},
+"Recipe18": {
+"Title":"Shrimp Curry ",
+"Serves":"(Serves 4) ",
+"Ingredients":
+[
+"2 tbsp Vegetable Oil ",
+"Whole Garam Masala ",
+"  1 Bayleaf ",
+"  4 Cloves ",
+"  3-9 Cardamom Pods ",
+"  4 Cinnamon Sticks ",
+"½ Onion (grated) ",
+"5 Garlic Cloves (crushed) ",
+"1in Ginger (grated) ",
+"1 tsp Paprika or Tumeric ",
+"1 tsp Cumin ",
+"¼ tsp Chili Powder ",
+"1 ½ cup Water ",
+"2 Potatoes (cut into eighths) ",
+"1 tsp Salt ",
+"1 tsp Sugar ",
+"½ tin/ 1 cup Coconut Milk ",
+"1lb Shrimp ",
+"¼ tsp Garam Masala (ground) ",
+"1 tsp Ghee or Butter "
+],
+"Directions":
+[
+"Heat oil in pressure cooker. ",
+"Fry whole garam masala in hot oil for a few seconds. ",
+"Add onion. Fry till brown. ",
+"Add garlic, and ginger. Fry for a few seconds or until dry. ",
+"Add paprika, cumin, and chili powder. Fry slightly longer (until dry). ",
+"Add water and potatoes. Boil. ",
+"Add salt, sugar, and coconut milk. Mix in. ",
+"Add shrimp. Mix and fry for a few seconds. ",
+"Pressure cook for 3 mins. ",
+"Release pressure. ",
+"Add ground garam masala and butter. ",
+"Mix and serve. "
+]},
+"Recipe19": {
+"Title":"Banana Bread ",
+"Serves":"(2 loaves) ",
+"Ingredients":
+[
+"2 cups Sugar ",
+"1 cup Butter ",
+"6 Bananas (ripe) ",
+"4 Eggs ",
+"2 ½ cups Cake Flour (or substitute) ",
+"2 tsp Baking Soda ",
+"1 tsp Salt "
+],
+"Directions":
+[
+"Cream butter and sugar. ",
+"Add beaten eggs and mashed bananas. Mix well. ",
+"Combine dry ingredients. ",
+"Add to banana mixture. Mix in. ",
+"Pour into 2 greased loaf pans. ",
+"Bake at degrees for 45-mins (until brown). "
+]},
+"Recipe20": {
+"Title":"English Scones ",
+"Serves":"(Makes 12) ",
+"Ingredients":
+[
+"1 ¾ cups All Purpose Flour 3 tbsp Sugar",
+"2 ½ tsp Baking Powder ",
+"½ tsp Salt ",
+"1/3 cup Butter ",
+"1 Egg (beaten) ",
+"½ cup Raisins ",
+"4 to 6 tbsp Milk "
+],
+"Directions":
+[
+"Heat oven to degrees. ",
+"Mix flour, sugar, baking powder and salt. ",
+"Cut butter into flour mixture. ",
+"Add raisins. ",
+"Stir in egg and just enough milk so dough leaves side of bowl. ",
+"Turn dough onto lightly floured surface. ",
+"Knead lightly times. ",
+"Roll to ½ “ thickness. ",
+"Cut with 2 inch cutter. ",
+"Place on ungreased cookie sheet. ",
+"Bake 10-mins. "
+]},
+}
+
+module.exports = IndianRecipes;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const cors = require("cors")
+const cooking = require("./IndianCooking2")
+
+const app = express()
+const PORT = 8000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/api/recipes', (req, res) => {
+  res.json(cooking)
+})
+
+app.listen(PORT, ()=>{
+  console.log(`Server running on port ${PORT}`)
+})
